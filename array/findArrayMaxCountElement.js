e8b1fe0cc9c2856c12a94e92f1e2507969a7a1b8
liaowenqiang828 <18840965351@163.com>
2019-12-14 14:19:07 +0800

    [Wenqiang Liao]{feat}: finish the task get the max count element of an array

--- array/findArrayMaxCountElement.js
+++ array/findArrayMaxCountElement.js
@@ -1,3 +1,28 @@
 var a = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
 
-function maxCountElement()
+function maxCountElement(array) {
+  var tempObj = {};
+  for (var i = 0; i < array.length; i++) {
+    var key = array[i];
+    if (tempObj[key]) {
+      tempObj[key]++;
+    }
+    else {
+      tempObj[key] = 1;
+    }
+  }
+
+  var maxCount = 0;
+  var maxElement = array[0];
+  
+  for (var key in tempObj) {
+    if (maxCount < tempObj[key]) {
+      maxCount = tempObj[key];
+      maxElement = key;
+    }
+  }
+  return maxElement;
+}
+
+var maxElementOfArray = maxCountElement(a);
+console.log(maxElementOfArray);
